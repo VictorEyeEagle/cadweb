27674b8c3cb334b1f647b569a6190299c8a63a2a
VictorEyeEagle <victor100bol@hotmail.com>
2025-01-15 01:36:36 -0300

    .

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import *
+from datetime import date
 
 class CategoriaForm(forms.ModelForm):
      class Meta:
@@ -12,30 +13,101 @@ class CategoriaForm(forms.ModelForm):
                'ordem': forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': 'Ordem'}),
           }
           labels = {
-               'nonme': 'Informe o nome do produto: ',
+               'nome': 'Informe o nome do produto: ',
                'ordem': 'Informe o número da ordem: ',
           }
 
      def clean_nome(self):
           nome = self.cleaned_data.get('nome')
+          if not self.instance.pk:  # Verifica se exite id, caso não exita verificar o valor colocado se já existe ou não
+               if Categoria.objects.filter(nome=nome).exists():
+                    raise forms.ValidationError("Já existe uma categoria com esse mesmo nome.")
           if len(nome) < 3:
-               raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
-          
-          # if Categoria.objects.filter(nome=nome).exists():
-          #      raise forms.ValidationError("Já existe uma categoria com esse mesmo nome.")
-          
+               raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")          
           return nome
      
      def clean_ordem(self):
           ordem = self.cleaned_data.get('ordem')
+          if not self.instance.pk:
+               if Categoria.objects.filter(ordem=ordem).exists():
+                    raise forms.ValidationError("O númera da ordem digitada já esta sendo utilizada.")
           if ordem <= 0:
                raise forms.ValidationError("O campo ordem tem que ser superior a 0.")
-          
-          # if Categoria.objects.filter(ordem=ordem).exists():
-          #      raise forms.ValidationError("O númera da ordem digitada já esta sendo utilizada.")
-          
           return ordem
+     
 
+class ClienteForm(forms.ModelForm):
+     class Meta:
+          model = Cliente 
+          fields = ['nome', 'cpf', 'datanasc',]
+          widgets = {
+               'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
+               'cpf':forms.TextInput(attrs={'class': 'cpf form-control', 'placeholder': 'C.P.F'}),
+               'datanasc':forms.DateInput(attrs={'class': 'data form-control', 'placeholder': 'Data de Nascimento'}, format='%d/%m/%Y'),
+          }
+
+     def clean_nome(self):
+         nome = self.cleaned_data.get('nome')
+        #  if Cliente.objects.filter(nome=nome).exclude(pk=self.instance.pk).exists():
+        #      raise forms.ValidationError("Já existe um cliente com esse nome.")
+         if len(nome) < 3:
+             raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+         return nome
+
+     def clean_cpf(self):
+         cpf = self.cleaned_data.get('cpf')
+         if Cliente.objects.filter(cpf=cpf).exclude(pk=self.instance.pk).exists():
+             raise forms.ValidationError("Já existe uma pessoa com esse C.P.F.")
+         if len(cpf) != 14:  # Exemplo: "000.000.000-00"
+             raise forms.ValidationError("O C.P.F. deve conter 11 caracteres.")
+         return cpf
+
+     def clean_datanasc(self):
+         datanasc = self.cleaned_data.get('datanasc')
+         if datanasc >= date.today():
+             raise forms.ValidationError("A data de nascimento não pode ser maior ou igual a data atual.")
+         return datanasc
+     
+
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'preco', 'categoria','img_base64']
+        widgets = {
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
+            'img_base64': forms.HiddenInput(), 
+            'preco':forms.TextInput(attrs={
+                'class': 'money form-control',
+                'maxlength': 500,
+                'placeholder': '0.000,00'
+            }),
+        }
+        
+        labels = {
+            'nome': 'Nome do Produto',
+            'preco': 'Preço do Produto',
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        super(ProdutoForm, self).__init__(*args, **kwargs)
+        self.fields['preco'].localize = True
+        self.fields['preco'].widget.is_localized = True 
+
+class EstoqueForm(forms.ModelForm):
+     class Meta:
+          model = Estoque
+          fields = ['produto', 'qtde']
+
+          widgets = {
+               'produto': forms.HiddenInput(),
+               'qtde': forms.TextInput(attrs={'class': 'inteiro form-control',}),
+          }  
+
+   
+
+     
      # def clean(self):
      #      cleaned_data = super().clean()
      #      nome = cleaned_data().get('nome')
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import *
 from .forms import *
+from datetime import date
 
 def index(request):
     return render(request,'index.html')
@@ -19,14 +20,13 @@ def form_categoria(request):
             salvando = form.save()
             lista=[]
             lista.append(salvando)
-            messages.success(request, 'Operação concluida!')
+            messages.success(request, 'Operação realizada com sucesso.')
             return render(request, 'categoria/lista.html', {'lista':lista,})
         
     else: 
         form = CategoriaForm()
     
     return render(request, 'categoria/formulario.html', {'form': form,})
-
 
 def editar_categoria(request, id):
     try:
@@ -48,19 +48,16 @@ def editar_categoria(request, id):
     
     return render(request, 'categoria/formulario.html', {'form':form,})
 
-
 def remover_categoria(request, id):
     try:
         categoria = Categoria.objects.get(pk=id)
         categoria.delete()
-        messages.success(request, 'Exclusão bem sucedida!')
+        messages.success(request, 'Exclusão realizada com sucesso.')
     except:
         messages.error(request, 'Registro não encontrado')
         return redirect('lista')
     
     return redirect('lista')
-    # return render(request, 'categoria/lista.html')
-
 
 def detalhe_categoria(request, id):
     try:
@@ -70,3 +67,141 @@ def detalhe_categoria(request, id):
         return redirect('lista')
 
     return render(request, 'categoria/detalhes.html', {'categoria':categoria,})
+
+def cliente(request):
+    contexto={
+        'listaCliente': Cliente.objects.all().order_by('-id')
+    }
+    return render(request,'cliente/lista.html', contexto)
+
+def form_cliente(request):
+    if (request.method == 'POST'):
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            salvando = form.save()
+            listaCliente=[]
+            listaCliente.append(salvando)
+            messages.success(request, 'Operação realizada com sucesso.')
+            return render(request, 'cliente/lista.html', {'listaCliente':listaCliente,})
+        
+    else: 
+        form = ClienteForm()
+    
+    return render(request, 'cliente/formulario.html', {'form': form,})
+
+def editar_cliente(request, id):
+    try:
+        cliente = Cliente.objects.get(pk=id)
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaCliente')
+
+    if (request.method == 'POST'):
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            cliente = form.save()
+            listaCliente=[]
+            listaCliente.append(cliente)
+            return render(request, 'cliente/lista.html', {'listaCliente':listaCliente,})
+
+    else: 
+        form = ClienteForm(instance=cliente)
+    
+    return render(request, 'cliente/formulario.html', {'form':form,})
+
+def remover_cliente(request, id):
+    try:
+        cliente = Cliente.objects.get(pk=id)
+        cliente.delete()
+        messages.success(request, 'Exclusão realizada com Sucesso.')
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaCliente')
+    
+    return redirect('listaCliente')
+
+def detalhe_cliente(request, id):
+    try:
+        cliente = get_object_or_404(Cliente, pk=id)
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaCliente')
+
+    return render(request, 'cliente/detalhes.html', {'cliente':cliente,})
+
+def produto(request):
+    contexto={
+        'listaProduto': Produto.objects.all().order_by('-id')
+    }
+    return render(request,'produto/lista.html', contexto)
+
+def form_produto(request):
+    if (request.method == 'POST'):
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            salvando = form.save()
+            listaProduto=[]
+            listaProduto.append(salvando)
+            messages.success(request, 'Operação realizada com sucesso.')
+            return render(request, 'produto/lista.html', {'listaProduto':listaProduto,})
+        
+    else: 
+        form = ProdutoForm()
+    
+    return render(request, 'produto/formulario.html', {'form': form,})
+
+
+def editar_produto(request, id):
+    try:
+        produto = Produto.objects.get(pk=id)
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaProduto')
+
+    if (request.method == 'POST'):
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            produto = form.save()
+            listaProduto=[]
+            listaProduto.append(produto)
+            return redirect('listaProduto')
+
+    else: 
+        form = ProdutoForm(instance=produto)
+    
+    return render(request, 'produto/formulario.html', {'form':form,})
+
+def remover_produto(request, id):
+    try:
+        produto = Produto.objects.get(pk=id)
+        produto.delete()
+        messages.success(request, 'Exclusão realizada com sucesso.')
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaProduto')
+    
+    return redirect('listaProduto')
+
+def detalhe_produto(request, id):
+    try:
+        produto = get_object_or_404(Produto, pk=id)
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('listaProduto')
+
+    return render(request, 'produto/detalhes.html', {'produto':produto,})
+
+
+def ajustar_estoque(request, id):
+    produto = Produto.objects.get(pk=id)
+    estoque = produto.estoque 
+    if request.method == 'POST':
+        form = EstoqueForm(request.POST, instance=estoque)
+        if form.is_valid():
+            estoque = form.save()
+            listaProduto = []
+            listaProduto.append(estoque.produto) 
+            return render(request, 'produto/lista.html', {'listaProduto': listaProduto})
+    else:
+         form = EstoqueForm(instance=estoque)
+    return render(request, 'produto/estoque.html', {'form': form,})
